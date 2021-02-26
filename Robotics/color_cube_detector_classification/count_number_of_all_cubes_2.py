604743584c37928f8fe26f9d9345b36c4af0dbef
RonaldsonBellande <ronaldsonbellande@gmail.com>
2021-02-26 15:58:39 -0500

    crop_almmost

--- Robotics/color_cube_detector_classification/count_number_of_all_cubes_2.py
+++ Robotics/color_cube_detector_classification/count_number_of_all_cubes_2.py
@@ -157,7 +157,7 @@ class detect_cube(object):
         combine_image = cv2.bitwise_and(img, img, mask = mask)
         combine_image[combine_image == 0] = 255
         
-        
+        # Crop image from white space
         gray = cv2.cvtColor(combine_image, cv2.COLOR_BGR2GRAY)
         th, threshed = cv2.threshold(gray, 240, 255, cv2.THRESH_BINARY_INV)
         
@@ -165,53 +165,108 @@ class detect_cube(object):
         morphed = cv2.morphologyEx(threshed, cv2.MORPH_CLOSE, kernel)
         
         contours = cv2.findContours(morphed,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
-        
-        if len(contours) > 0:
-            cnt = sorted(contours, key=cv2.contourArea, reverse=True)[-1]
+        count = 0
+        if len(contours) != 0:
+            count = 0
+            for i in range(len(contours)):
+                cnt = sorted(contours, key=cv2.contourArea, reverse=True)[-1]
+                
+                # The amount of object found
+                if cv2.contourArea(cnt) > 2:
+                    count +=1
+                    
+                    if count == 1:
+                        x,y,w,h = cv2.boundingRect(cnt)
+                        combine_image = combine_image[y:y+h, x:x+w]
+                    elif count == 2:
+                        x_2,y_2,w_2,h_2 = cv2.boundingRect(cnt)
+                        combine_image = combine_image[y_2:y_2+h_2, x_2:x_2+w_2]
+                
+                else:
+                    combine_image = cv2.bitwise_and(img, img, mask = mask)
+                    combine_image[combine_image == 0] = 255
+                    
         else:
-            print("Sorry No contour Found.")
-
-        
-        x,y,w,h = cv2.boundingRect(cnt)
-        combine_image = combine_image[y:y+h, x:x+w]
-        
-        
+            combine_image = cv2.bitwise_and(img, img, mask = mask)
+            combine_image[combine_image == 0] = 255
+            
+        if combine_image == []:
+            combine_image = cv2.bitwise_and(img, img, mask = mask)
+            combine_image[combine_image == 0] = 255
+    
+    
         if color == 'yellow':
             image_output = "create_model_from_cubes_2/yellow_cubes/"
         elif color == 'green':
             image_output = "create_model_from_cubes_2/green_cubes/"
         else:
             image_output = "create_model_from_cubes_2/red_cubes/"
-
+        
         if number != 0:
-            for i in range(len(image_number)):
-                cv2.imwrite(os.path.join(image_output, str(file_name)), combine_image)
+            if count == 1 or count == 0:
                 
-            image_rotate = ndimage.rotate(combine_image, 60)
-            image_rotate[image_rotate == 0] = 255
-            for i in range(len(image_number)):
-                cv2.imwrite(os.path.join(image_output, "rotate_1" + str(file_name)), image_rotate)
-            
-            image_rotate = ndimage.rotate(combine_image, 120)
-            image_rotate[image_rotate == 0] = 255
-            for i in range(len(image_number)):
-                cv2.imwrite(os.path.join(image_output, "rotate_2" + str(file_name)), image_rotate)
-            
-            image_rotate = ndimage.rotate(combine_image, 180)
-            image_rotate[image_rotate == 0] = 255
-            for i in range(len(image_number)):
-                cv2.imwrite(os.path.join(image_output, "rotate_3" + str(file_name)), image_rotate)
-            
-            image_rotate = ndimage.rotate(combine_image, 240)
-            image_rotate[image_rotate == 0] = 255
-            for i in range(len(image_number)):
-                cv2.imwrite(os.path.join(image_output, "rotate_4" + str(file_name)), image_rotate)
-            
-            image_rotate = ndimage.rotate(combine_image, 300)
-            image_rotate[image_rotate == 0] = 255
-            for i in range(len(image_number)):
-                cv2.imwrite(os.path.join(image_output, "rotate_5" + str(file_name)), image_rotate)
-                cv2.waitKey(0)
+                for i in range(len(image_number)):
+                    cv2.imwrite(os.path.join(image_output, str(file_name)), combine_image)
                 
+                image_rotate = ndimage.rotate(combine_image, 60)
+                image_rotate[image_rotate == 0] = 255
+                for i in range(len(image_number)):
+                    cv2.imwrite(os.path.join(image_output, "rotate_1" + str(file_name)), image_rotate)
+            
+                image_rotate = ndimage.rotate(combine_image, 120)
+                image_rotate[image_rotate == 0] = 255
+                for i in range(len(image_number)):
+                    cv2.imwrite(os.path.join(image_output, "rotate_2" + str(file_name)), image_rotate)
+            
+                image_rotate = ndimage.rotate(combine_image, 180)
+                image_rotate[image_rotate == 0] = 255
+                for i in range(len(image_number)):
+                    cv2.imwrite(os.path.join(image_output, "rotate_3" + str(file_name)), image_rotate)
+            
+                image_rotate = ndimage.rotate(combine_image, 240)
+                image_rotate[image_rotate == 0] = 255
+                for i in range(len(image_number)):
+                    cv2.imwrite(os.path.join(image_output, "rotate_4" + str(file_name)), image_rotate)
+            
+                image_rotate = ndimage.rotate(combine_image, 300)
+                image_rotate[image_rotate == 0] = 255
+                for i in range(len(image_number)):
+                    cv2.imwrite(os.path.join(image_output, "rotate_5" + str(file_name)), image_rotate)
+                    cv2.waitKey(0)
+            
+            elif count == 2:
+                
+                combine_image = cv2.bitwise_and(img, img, mask = mask)
+                combine_image[combine_image == 0] = 255
+                print(combine_image)
+                
+                for i in range(len(image_number)):
+                    cv2.imwrite(os.path.join(image_output, "normal_2" + str(file_name)), combine_image)
+                
+                image_rotate = ndimage.rotate(combine_image, 60)
+                image_rotate[image_rotate == 0] = 255
+                for i in range(len(image_number)):
+                    cv2.imwrite(os.path.join(image_output, "rotate_1_2" + str(file_name)), image_rotate)
+            
+                image_rotate = ndimage.rotate(combine_image, 120)
+                image_rotate[image_rotate == 0] = 255
+                for i in range(len(image_number)):
+                    cv2.imwrite(os.path.join(image_output, "rotate_2_2" + str(file_name)), image_rotate)
+            
+                image_rotate = ndimage.rotate(combine_image, 180)
+                image_rotate[image_rotate == 0] = 255
+                for i in range(len(image_number)):
+                    cv2.imwrite(os.path.join(image_output, "rotate_3_2" + str(file_name)), image_rotate)
+            
+                image_rotate = ndimage.rotate(combine_image, 240)
+                image_rotate[image_rotate == 0] = 255
+                for i in range(len(image_number)):
+                    cv2.imwrite(os.path.join(image_output, "rotate_4_2" + str(file_name)), image_rotate)
+            
+                image_rotate = ndimage.rotate(combine_image, 300)
+                image_rotate[image_rotate == 0] = 255
+                for i in range(len(image_number)):
+                    cv2.imwrite(os.path.join(image_output, "rotate_5_2" + str(file_name)), image_rotate)
+                    cv2.waitKey(0)
                         
     
